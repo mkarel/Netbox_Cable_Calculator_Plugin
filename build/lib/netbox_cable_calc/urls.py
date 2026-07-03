e41cc1ea4ffc44355a0b8f63772bec9e8399e947
Matt Karel <mkarel@gmail.com>
2026-07-03 14:17:37 -0400

    Cleaing up the repository by removing unnecessary files and directories.

--- build/lib/netbox_cable_calc/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.CalculatorView.as_view(), name="calculator"),
-    path("bom/", views.BomApiView.as_view(),     name="bom-api"),
-    path("layout/",   views.LayoutApiView.as_view(),  name="layout-api"),
-]
